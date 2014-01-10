47d70fbfac99125ec62fd4e35551b25580ab45ee
Derek Prior <derekprior@gmail.com>
2014-01-09 20:29:30 -0500

    Use octokit

--- watchme.rb
+++ watchme.rb
@@ -5,57 +5,26 @@
 #
 ###
 require 'json'
+require 'octokit'
 
 class GitHub
   def initialize(username, password)
-    @username = username
-    @password = password
-  end
-
-  def repos
-    get_all(repos_url).map { |repo| repo['full_name'] }
-  end
-
-  def subscriptions
-    get_all(subscriptions_url).map { |repo| repo['full_name'] }
-  end
-
-  private
-
-  attr_reader :username, :password
-
-  def repos_url
-    user_json['repos_url']
-  end
-
-  def subscriptions_url
-    user_json['subscriptions_url']
-  end
-
-  def user_json
-    @user_json ||= get(user_url.sub('{user}', @username))
-  end
-
-  def user_url
-    root_json['user_url']
-  end
-
-  def root_json
-    @root_json ||= get('https://api.github.com')
-  end
-
-  def get_all(url, page = 1)
-    objects = get("#{url}?per_page=100&page=#{page}")
-
-    if objects.empty?
-      objects
-    else
-      objects + get_all(url, page + 1)
+    Octokit.configure do |config|
+      config.login = username
+      config.password = password
     end
   end
 
-  def get(url)
-    JSON.parse(`curl --silent -u '#{username}:#{password}' '#{url}'`)
+  def repositories
+    @repos ||= Octokit.repositories
+  end
+
+  def subscriptions
+    @subscriptions ||= Octokit.subscriptions
+  end
+
+  def unwatched
+    @unwatched ||= repositories - subscriptions
   end
 end
 
@@ -73,14 +42,12 @@ end
 
 github = GitHub.new(username, password)
 
-watchme = github.repos - github.subscriptions
-
-if watchme.any?
+if github.unwatched.any?
   puts "You own but are not watching the following:"
 
-  watchme.each do |repo|
-    puts "https://github.com/#{repo}"
+  github.unwatched.each do |repo|
+    puts "https://github.com/#{repo.full_name}"
   end
 else
-  puts "You're watching all your own repos. Nice."
+  Octokit.say "You're watching all your own repos. Nice."
 end
